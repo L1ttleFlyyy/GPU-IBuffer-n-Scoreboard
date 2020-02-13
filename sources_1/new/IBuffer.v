9c2001acc4db69dc33192b724603b0e0d4587863
Chang Xu <920114525@qq.com>
2020-02-12 22:30:59 -0800

    Scoreboard passed synth

--- sources_1/new/IBuffer.v
+++ sources_1/new/IBuffer.v
@@ -72,8 +72,7 @@ module IBuffer_warp#(
 
     // signal to/from Operand Collector
     output valid_IB_OC,
-    output valid_IB_OC,
-    output [31:0]instr_OC_OC,
+    output [31:0]instr_IB_OC,
     output [5:0]src1_IB_OC, // 5-bit RegID with MSB as valid
     output [5:0]src2_IB_OC,
     output [5:0]dst_IB_OC,
@@ -85,20 +84,95 @@ module IBuffer_warp#(
     output shared_globalbar_IB_OC,
     output BEQ_IB_OC_SIMT,
     output BLT_IB_OC_SIMT,
-    output exit_IB_OC,
 
-    // signal to RAU TODO: this pair of signal actually come from IU
-    output exit_IB_RAU,
-    output [LOGNUM_WARPS-1: 0]warpID_IB_RAU,
-    input [Instruction_Width-1: 0] Instruction_ID1_IB,
-    input [Instruction_Width-1: 0] Instruction_ID2_IB,
-    input valid_ID1_IB,
-    input valid_ID2_IB,
-    input [7:0] mask_SIMT_IB,
-    input drop_SIMT_IB,
-    input issue_grant, // from Issue unit
-    output req_IB_IU,
-    output req_IB_IF,
-    output [Instruction_Width-1: 0] Instruction_IB_OC
+    // signal to RAU
+    output exit_IB_RAU
     );
+
+endmodule
+
+module IBuffer_wrapper#(
+    parameter NUM_WARPS = 8,
+    parameter NUM_THREADS = 8,
+    parameter LOGNUM_WARPS = $clog2(NUM_WARPS)
+    ) (
+    input clk,
+    input rst,
+
+    // signals to/from IF stage (warp specific)
+    input [NUM_WARPS-1: 0]valid_IF_IB, 
+    output [NUM_WARPS-1: 0]req_IB_IF,
+    
+    // signals from SIMT (warp specific)
+    input [NUM_WARPS-1: 0]drop_SIMT_IB,
+    input [NUM_WARPS*NUM_THREADS-1: 0]mask_flattened_SIMT_IB,
+
+    // signals to/from scoreboard (warp specific)
+    output [5*NUM_WARPS-1: 0] src1_Flattened_IB_Scb, // Flattened RegID: 5 bit regID * 8 waprs
+    output [5*NUM_WARPS-1: 0] src2_Flattened_IB_Scb,
+    output [5*NUM_WARPS-1: 0] dst_Flattened_IB_Scb,
+    input [2*NUM_WARPS-1: 0] ScbID_Flattened_Scb_IB, // ScbID to be passed to Operand Collector
+    input [NUM_WARPS-1: 0] src1_valid,
+    input [NUM_WARPS-1: 0] src2_valid,
+    input [NUM_WARPS-1: 0] dst_valid,
+    output [NUM_WARPS-1: 0] full_Scb_IB,
+    output [NUM_WARPS-1: 0] dependent_Scb_IB,
+
+    // signals from ID stage (dual decoding unit)
+    input valid_Q1_ID_IB,
+    input [31:0]instr_Q1_ID_IB,
+    input [5:0]src1_Q1_ID_IB, // 5-bit RegID with MSB as valid
+    input [5:0]src2_Q1_ID_IB,
+    input [5:0]dst_Q1_ID_IB,
+    input [3:0]ALUop_Q1_ID_IB,
+    input [15:0]imme_Q1_ID_IB,
+    input regwrite_Q1_ID_IB,
+    input memwrite_Q1_ID_IB,
+    input memread_Q1_ID_IB,
+    input shared_globalbar_Q1_ID_IB,
+    input BEQ_Q1_ID_IB_SIMT,
+    input BLT_Q1_ID_IB_SIMT,
+    input exit_Q1_ID_IB,
+
+    input valid_Q2_ID_IB,
+    input [31:0]instr_Q2_ID_IB,
+    input [5:0]src1_Q2_ID_IB, // 5-bit RegID with MSB as valid
+    input [5:0]src2_Q2_ID_IB,
+    input [5:0]dst_Q2_ID_IB,
+    input [3:0]ALUop_Q2_ID_IB,
+    input [15:0]imme_Q2_ID_IB,
+    input regwrite_Q2_ID_IB,
+    input memwrite_Q2_ID_IB,
+    input memread_Q2_ID_IB,
+    input shared_globalbar_Q2_ID_IB,
+    input BEQ_Q2_ID_IB_SIMT,
+    input BLT_Q2_ID_IB_SIMT,
+    input exit_Q2_ID_IB,
+
+    // signal to/from Operand Collector
+    output valid_IB_OC,
+    output [31:0]instr_IB_OC,
+    output [LOGNUM_WARPS-1: 0]warpID_IB_OC,
+    output [1:0]ScbID_IB_OC,
+    output [5:0]src1_IB_OC, // 5-bit RegID with MSB as valid
+    output [5:0]src2_IB_OC,
+    output [5:0]dst_IB_OC,
+    output [15:0]imme_IB_OC,
+    output [3:0]ALUop_IB_OC,
+    output regwrite_IB_OC,
+    output memwrite_IB_OC,
+    output memread_IB_OC,
+    output shared_globalbar_IB_OC,
+    output BEQ_IB_OC_SIMT,
+    output BLT_IB_OC_SIMT,
+
+    // signal to RAU
+    output exit_IB_RAU,
+    output [LOGNUM_WARPS-1: 0]warpID_IB_RAU
+    );
+    wire [NUM_WARPS-1: 0]req_IB_IU;
+    wire [NUM_WARPS-1: 0]grt_IU_IB;
+    
+    // signal for scoreboard clearing
+
 endmodule
--- sources_1/new/scoreboard.v
+++ sources_1/new/scoreboard.v
@@ -23,22 +23,27 @@
 module scoreboard_warp(
     input clk,
     input rst,
+    // signal from IBuffer when issuing
     input [4:0] src1, // RegID is 5-bit (R8: thrID, R16: warpID)
     input [4:0] src2,
     input [4:0] dst,
     input src1_valid,
     input src2_valid,
     input dst_valid,
-    input issue_grant,
-    input [1:0] ScbID_MEM_Scb, // clear signal from MEM (SW only)
-    input [1:0] ScbID_ALU_Scb, // clear signal from ALU (branch only)
-    input [1:0] ScbID_CDB_Scb, // clear signal from CDB (for all regwrite)
-    input ScbID_valid_MEM_Scb,
-    input ScbID_valid_ALU_Scb,
-    input ScbID_valid_CDB_Scb,
+    input RP_grant, // only create Scb entry for RP_grant (avoid duplicate entry for replay instructions)
+    input replayable, // if it is LW/SW, the Scb entry will be marked as "incomplete"
+    //signal for clearing
+    input [1:0] replay_complete_ScbID, // mark the Scb entry as complete
+    input replay_complete,
+    input replay_SW_LWbar, // distinguish between SW/LW
+    // signal from other modules
+    input [1:0] clear_ScbID_Br, // clear signal from ALU (for branch only)
+    input [1:0] clear_ScbID_regwr, // clear signal from CDB (for all regwrite)
+    input clear_valid_Br,
+    input clear_valid_regwr,
     output full,
     output dependent,
-    output [1:0] ScbID_Scb_OC // ScbID passed to Operand Collector (for future clearing)
+    output [1:0] ScbID_Scb_IB // ScbID passed to IBuffer (for future clearing)
     );
     reg [4:0] src1_array [3:0];
     reg [4:0] src2_array [3:0];
@@ -47,14 +52,18 @@ module scoreboard_warp(
     reg [3:0] src2_valid_array;
     reg [3:0] dst_valid_array;
     reg [3:0] valid_array;
+    reg [3:0] replay_complete_array;
 
-    // clear signal received from Mem/ALU/CDB?
+    // valid array after Scb entries cleared by Mem/ALU/CDB
     reg [3:0] valid_array_cleared;
     always@(*) begin
         valid_array_cleared = valid_array;
-        if (ScbID_valid_MEM_Scb) valid_array_cleared[ScbID_MEM_Scb] = 0;
-        if (ScbID_valid_ALU_Scb) valid_array_cleared[ScbID_ALU_Scb] = 0;
-        if (ScbID_valid_CDB_Scb) valid_array_cleared[ScbID_CDB_Scb] = 0;
+        if (replay_complete & replay_SW_LWbar) // from IBuffer for SW
+            valid_array_cleared[replay_complete_ScbID] = 0;
+        if (clear_valid_regwr & replay_complete_array[clear_ScbID_regwr]) // from CDB
+            valid_array_cleared[clear_ScbID_regwr] = 0; // clear only when finished (in case of LW)
+        if (clear_valid_Br) // from ALU for branch
+            valid_array_cleared[clear_ScbID_Br] = 0;
     end
     assign full = &valid_array_cleared;
 
@@ -69,7 +78,7 @@ module scoreboard_warp(
                 next_empty = i;
         end
     end
-    assign ScbID_Scb_OC = next_empty;
+    assign ScbID_Scb_IB = next_empty;
 
     // store a new instruction if granted by Issue unit
     always@(posedge clk or negedge rst) begin
@@ -77,18 +86,26 @@ module scoreboard_warp(
             valid_array <= 0;
         end else begin
             valid_array <= valid_array_cleared;
-            if (issue_grant)
-                valid_array[next_empty] <= 1;      
+            if (RP_grant)
+                valid_array[next_empty] <= 1'b1;      
         end
     end
+
     always@(posedge clk) begin
-        if (issue_grant) begin
+        if (RP_grant) begin
             src1_array[next_empty] <= src1;
             src2_array[next_empty] <= src2;
             dst_array[next_empty] <= dst;
             src1_valid_array[next_empty] <= src1_valid;
             src2_valid_array[next_empty] <= src2_valid;
             dst_valid_array[next_empty] <= dst_valid;
+            if (replayable) 
+                replay_complete_array[next_empty] <= 1'b0;
+            else
+                replay_complete_array[next_empty] <= 1'b1;
+        end
+        if (replay_complete) begin
+            replay_complete_array[replay_complete_ScbID] <= 1'b1;
         end
     end
 
@@ -118,44 +135,87 @@ endmodule // scoreboard_inner
 
 module scoreboard#(
     parameter NUM_WARPS = 8,
-    parameter NUM_WARPS = $clog2(NUM_WARPS)
+    parameter LOGNUM_WARPS = $clog2(NUM_WARPS)
     ) (
     // common signals
     input clk,
     input rst,
-    input [3: 0] ScbID_MEM_Scb, // clear signal from MEM (SW only)
-    input [3: 0] ScbID_ALU_Scb, // clear signal from ALU (branch only)
-    input [3: 0] ScbID_CDB_Scb, // clear signal from CDB (for all regwrite)
-    input ScbID_valid_MEM_Scb,
-    input ScbID_valid_ALU_Scb,
-    input ScbID_valid_CDB_Scb,
+    input [1:0] clear_ScbID_ALU_Scb, // clear signal from ALU (branch only)
+    input [1:0] clear_ScbID_CDB_Scb, // clear signal from CDB (for all regwrite)
+    input [LOGNUM_WARPS-1:0] clear_warpID_ALU_Scb,
+    input [LOGNUM_WARPS-1:0] clear_warpID_CDB_Scb,
+    input clear_valid_ALU_Scb,
+    input clear_valid_CDB_Scb,
 
     // warp specific signals
-    input [NUM_WARPS-1: 0] grant_IU_IB_Scb, // grant from issue unit
-    input [5*NUM_WARPS-1: 0] src1_Flattened_IB_Scb, // Flattened RegID: 5 bit regID * 8 waprs
-    input [5*NUM_WARPS-1: 0] src2_Flattened_IB_Scb,
-    input [5*NUM_WARPS-1: 0] dst_Flattened_IB_Scb,
-    output [2*NUM_WARPS-1: 0] ScbID_Flattened_Scb_IB, // ScbID to be passed to Operand Collector
-    input [NUM_WARPS-1: 0] src1_valid,
-    input [NUM_WARPS-1: 0] src2_valid,
-    input [NUM_WARPS-1: 0] dst_valid,
-    output [NUM_WARPS-1: 0] full_Scb_IB,
-    output [NUM_WARPS-1: 0] dependent_Scb_IB
+    // from IBuffer when depositing
+    input [NUM_WARPS-1:0] RP_grant_IB_Scb,
+    input [5*NUM_WARPS-1:0] src1_flattened_IB_Scb, // flattened RegID: 5 bit regID * 8 warps
+    input [5*NUM_WARPS-1:0] src2_flattened_IB_Scb,
+    input [5*NUM_WARPS-1:0] dst_flattened_IB_Scb,
+    input [NUM_WARPS-1:0] src1_valid_IB_Scb,
+    input [NUM_WARPS-1:0] src2_valid_IB_Scb,
+    input [NUM_WARPS-1:0] dst_valid_IB_Scb,
+    input [NUM_WARPS-1:0] replayable_IB_Scb,
+    // from IBuffer when clearing
+    input [2*NUM_WARPS-1:0] replay_complete_ScbID_flattened_IB_Scb,
+    input [NUM_WARPS-1:0] replay_complete_IB_Scb,
+    input [NUM_WARPS-1:0] replay_SW_LWbar_IB_Scb,
+    // to IBuffer when 
+    output [NUM_WARPS-1:0] full_Scb_IB,
+    output [NUM_WARPS-1:0] dependent_Scb_IB,
+    output [2*NUM_WARPS-1:0] ScbID_flattened_Scb_IB
     );
-
     wire [4:0] src1_IB_Scb [0:NUM_WARPS-1];
     wire [4:0] src2_IB_Scb [0:NUM_WARPS-1];
     wire [4:0] dst_IB_Scb [0:NUM_WARPS-1];
-    wire [3:0] ScbID_Scb_IB[0:NUM_WARPS-1];
+    wire [1:0] ScbID_Scb_IB [0:NUM_WARPS-1];
+    wire [1:0] replay_complete_ScbID_IB_Scb [0:NUM_WARPS-1];
+    reg [NUM_WARPS-1:0]clear_valid_ALU_Scb_array;
+    reg [NUM_WARPS-1:0]clear_valid_CDB_Scb_array;
+
+    // demux for the common signals
+    always@(*) begin
+        clear_valid_ALU_Scb_array = 0;
+        clear_valid_ALU_Scb_array[clear_warpID_ALU_Scb] = clear_valid_ALU_Scb;
+        clear_valid_CDB_Scb_array = 0;
+        clear_valid_CDB_Scb_array[clear_warpID_CDB_Scb] = clear_valid_CDB_Scb;
+    end
 
     // flatten and unflatten
     genvar i;
     generate
     for (i=0; i<NUM_WARPS; i=i+1) begin: scoreboard_loop
-        assign src1_IB_Scb[i] = src1_Flattened_IB_Scb[i*5:i*5+4];
-        assign src2_IB_Scb[i] = src2_Flattened_IB_Scb[i*5:i*5+4];
-        assign dst_IB_Scb[i] = dst_Flattened_IB_Scb[i*5:i*5+4];
-        assign ScbID_Flattened_Scb_IB[i*4+3:i*4] = ScbID_Scb_IB[i];
+        assign src1_IB_Scb[i] = src1_flattened_IB_Scb[i*5+4:i*5];
+        assign src2_IB_Scb[i] = src2_flattened_IB_Scb[i*5+4:i*5];
+        assign dst_IB_Scb[i] = dst_flattened_IB_Scb[i*5+4:i*5];
+        assign replay_complete_ScbID_IB_Scb[i] = replay_complete_ScbID_flattened_IB_Scb[2*i+1:2*i];
+        assign ScbID_flattened_Scb_IB[2*i+1:2*i] = ScbID_Scb_IB[i];
+        scoreboard_warp scb (
+            .clk(clk),
+            .rst(rst),
+            // signal from IBuffer when depositing
+            .src1(src1_IB_Scb[i]),
+            .src2(src2_IB_Scb[i]),
+            .dst(dst_IB_Scb[i]),
+            .src1_valid(src1_valid_IB_Scb[i]),
+            .src2_valid(src2_valid_IB_Scb[i]),
+            .dst_valid(dst_valid_IB_Scb[i]),
+            .RP_grant(RP_grant_IB_Scb[i]), // only create Scb entry for RP_grant (avoid duplicate entry for replay instructions)
+            .replayable(replayable_IB_Scb[i]), // if it is LW, the Scb entry will be marked as "incomplete"
+            // signal from IBuffer when clearing
+            .replay_complete_ScbID(replay_complete_ScbID_IB_Scb[i]), // mark the Scb entry as complete
+            .replay_complete(replay_complete_IB_Scb[i]),
+            .replay_SW_LWbar(replay_SW_LWbar_IB_Scb[i]),
+            // signal from other modules
+            .clear_ScbID_Br(clear_ScbID_ALU_Scb), // common signals
+            .clear_ScbID_regwr(clear_ScbID_CDB_Scb),
+            .clear_valid_Br(clear_valid_ALU_Scb_array[i]), // after demux
+            .clear_valid_regwr(clear_valid_CDB_Scb_array[i]),
+            .full(full_Scb_IB[i]),
+            .dependent(dependent_Scb_IB[i]),
+            .ScbID_Scb_IB(ScbID_Scb_IB[i]) // ScbID passed to Operand Collector (for future clearing)
+        ); 
     end
     endgenerate
     
