74666a87e58efb94d36b81ea55b1a1c10db76647
Chang Xu <920114525@qq.com>
2020-02-12 16:27:02 -0800

    naming convention

--- sources_1/new/scoreboard.v
+++ sources_1/new/scoreboard.v
@@ -23,38 +23,38 @@
 module scoreboard_warp(
     input clk,
     input rst,
-    input [4:0] Src1, // RegID is 5-bit (R8: thrID, R16: warpID)
-    input [4:0] Src2,
-    input [4:0] Dst,
-    input Src1_Valid,
-    input Src2_Valid,
-    input Dst_Valid,
+    input [4:0] src1, // RegID is 5-bit (R8: thrID, R16: warpID)
+    input [4:0] src2,
+    input [4:0] dst,
+    input src1_valid,
+    input src2_valid,
+    input dst_valid,
     input issue_grant,
     input [1:0] ScbID_MEM_Scb, // clear signal from MEM (SW only)
     input [1:0] ScbID_ALU_Scb, // clear signal from ALU (branch only)
     input [1:0] ScbID_CDB_Scb, // clear signal from CDB (for all regwrite)
-    input ScbID_Valid_MEM_Scb,
-    input ScbID_Valid_ALU_Scb,
-    input ScbID_Valid_CDB_Scb,
+    input ScbID_valid_MEM_Scb,
+    input ScbID_valid_ALU_Scb,
+    input ScbID_valid_CDB_Scb,
     output full,
     output dependent,
     output [1:0] ScbID_Scb_OC // ScbID passed to Operand Collector (for future clearing)
     );
-    reg [4:0] Src1_array [3:0];
-    reg [4:0] Src2_array [3:0];
-    reg [4:0] Dst_array [3:0];
-    reg [3:0] Src1_Valid_array;
-    reg [3:0] Src2_Valid_array;
-    reg [3:0] Dst_Valid_array;
+    reg [4:0] src1_array [3:0];
+    reg [4:0] src2_array [3:0];
+    reg [4:0] dst_array [3:0];
+    reg [3:0] src1_valid_array;
+    reg [3:0] src2_valid_array;
+    reg [3:0] dst_valid_array;
     reg [3:0] valid_array;
 
     // clear signal received from Mem/ALU/CDB?
     reg [3:0] valid_array_cleared;
     always@(*) begin
         valid_array_cleared = valid_array;
-        if (ScbID_Valid_MEM_Scb) valid_array_cleared[ScbID_MEM_Scb] = 0;
-        if (ScbID_Valid_ALU_Scb) valid_array_cleared[ScbID_ALU_Scb] = 0;
-        if (ScbID_Valid_CDB_Scb) valid_array_cleared[ScbID_CDB_Scb] = 0;
+        if (ScbID_valid_MEM_Scb) valid_array_cleared[ScbID_MEM_Scb] = 0;
+        if (ScbID_valid_ALU_Scb) valid_array_cleared[ScbID_ALU_Scb] = 0;
+        if (ScbID_valid_CDB_Scb) valid_array_cleared[ScbID_CDB_Scb] = 0;
     end
     assign full = &valid_array_cleared;
 
@@ -83,12 +83,12 @@ module scoreboard_warp(
     end
     always@(posedge clk) begin
         if (issue_grant) begin
-            Src1_array[next_empty] <= Src1;
-            Src2_array[next_empty] <= Src2;
-            Dst_array[next_empty] <= Dst;
-            Src1_Valid_array[next_empty] <= Src1_Valid;
-            Src2_Valid_array[next_empty] <= Src2_Valid;
-            Dst_Valid_array[next_empty] <= Dst_Valid;
+            src1_array[next_empty] <= src1;
+            src2_array[next_empty] <= src2;
+            dst_array[next_empty] <= dst;
+            src1_valid_array[next_empty] <= src1_valid;
+            src2_valid_array[next_empty] <= src2_valid;
+            dst_valid_array[next_empty] <= dst_valid;
         end
     end
 
@@ -99,15 +99,15 @@ module scoreboard_warp(
         for (i = 0; i < 4; i = i+1) begin: dependent_loop
             // RAW:
             dependent_array[i] = 
-                (Src1_Valid && Src1_Valid_array[i] && (Src1 == Dst_array[i])) | 
-                (Src2_Valid && Src2_Valid_array[i] && (Src2 == Dst_array[i]));
+                (src1_valid && src1_valid_array[i] && (src1 == dst_array[i])) | 
+                (src2_valid && src2_valid_array[i] && (src2 == dst_array[i]));
             // WAW:
             dependent_array[i] = dependent_array[i] |
-                (Dst_Valid && Dst_Valid_array[i] && (Dst == Dst_array[i]));
+                (dst_valid && dst_valid_array[i] && (dst == dst_array[i]));
             // WAR:
             dependent_array[i] = dependent_array[i] |
-                (Dst_Valid && Src1_Valid_array[i] && (Dst == Src1_array[i])) |
-                (Dst_Valid && Src2_Valid_array[i] && (Dst == Src2_array[i])); 
+                (dst_valid && src1_valid_array[i] && (dst == src1_array[i])) |
+                (dst_valid && src2_valid_array[i] && (dst == src2_array[i])); 
         end
         dependent_array = dependent_array & valid_array_cleared; // Note here use valid_cleared to save one clock
     end
@@ -126,35 +126,35 @@ module scoreboard#(
     input [3: 0] ScbID_MEM_Scb, // clear signal from MEM (SW only)
     input [3: 0] ScbID_ALU_Scb, // clear signal from ALU (branch only)
     input [3: 0] ScbID_CDB_Scb, // clear signal from CDB (for all regwrite)
-    input ScbID_Valid_MEM_Scb,
-    input ScbID_Valid_ALU_Scb,
-    input ScbID_Valid_CDB_Scb,
+    input ScbID_valid_MEM_Scb,
+    input ScbID_valid_ALU_Scb,
+    input ScbID_valid_CDB_Scb,
 
     // warp specific signals
-    input [NUM_WARPS-1: 0] Grant_IU_IB_Scb, // grant from issue unit
-    input [5*NUM_WARPS-1: 0] Src1_Flattened_IB_Scb, // Flattened RegID: 5 bit regID * 8 waprs
-    input [5*NUM_WARPS-1: 0] Src2_Flattened_IB_Scb,
-    input [5*NUM_WARPS-1: 0] Dst_Flattened_IB_Scb,
+    input [NUM_WARPS-1: 0] grant_IU_IB_Scb, // grant from issue unit
+    input [5*NUM_WARPS-1: 0] src1_Flattened_IB_Scb, // Flattened RegID: 5 bit regID * 8 waprs
+    input [5*NUM_WARPS-1: 0] src2_Flattened_IB_Scb,
+    input [5*NUM_WARPS-1: 0] dst_Flattened_IB_Scb,
     output [2*NUM_WARPS-1: 0] ScbID_Flattened_Scb_IB, // ScbID to be passed to Operand Collector
-    input [NUM_WARPS-1: 0] Src1_Valid,
-    input [NUM_WARPS-1: 0] Src2_Valid,
-    input [NUM_WARPS-1: 0] Dst_Valid,
-    output [NUM_WARPS-1: 0] Full_Scb_IB,
-    output [NUM_WARPS-1: 0] Dependent_Scb_IB
+    input [NUM_WARPS-1: 0] src1_valid,
+    input [NUM_WARPS-1: 0] src2_valid,
+    input [NUM_WARPS-1: 0] dst_valid,
+    output [NUM_WARPS-1: 0] full_Scb_IB,
+    output [NUM_WARPS-1: 0] dependent_Scb_IB
     );
 
-    wire [4:0] Scr1_IB_Scb [0:NUM_WARPS-1];
-    wire [4:0] Scr2_IB_Scb [0:NUM_WARPS-1];
-    wire [4:0] Dst_IB_Scb [0:NUM_WARPS-1];
+    wire [4:0] src1_IB_Scb [0:NUM_WARPS-1];
+    wire [4:0] src2_IB_Scb [0:NUM_WARPS-1];
+    wire [4:0] dst_IB_Scb [0:NUM_WARPS-1];
     wire [3:0] ScbID_Scb_IB[0:NUM_WARPS-1];
 
     // flatten and unflatten
     genvar i;
     generate
     for (i=0; i<NUM_WARPS; i=i+1) begin: scoreboard_loop
-        assign Src1_IB_Scb[i] = Src1_Flattened_IB_Scb[i*5:i*5+4];
-        assign Src2_IB_Scb[i] = Src2_Flattened_IB_Scb[i*5:i*5+4];
-        assign Dst_IB_Scb[i] = Dst_Flattened_IB_Scb[i*5:i*5+4];
+        assign src1_IB_Scb[i] = src1_Flattened_IB_Scb[i*5:i*5+4];
+        assign src2_IB_Scb[i] = src2_Flattened_IB_Scb[i*5:i*5+4];
+        assign dst_IB_Scb[i] = dst_Flattened_IB_Scb[i*5:i*5+4];
         assign ScbID_Flattened_Scb_IB[i*4+3:i*4] = ScbID_Scb_IB[i];
     end
     endgenerate
