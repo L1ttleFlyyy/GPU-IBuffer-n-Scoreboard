e69ab4732c8d14ac0fdb76c4d088426a5c7291a5
Chang <cxu925@usc.edu>
2020-02-14 02:15:00 -0800

    Interface between IBuffer and MEM

--- sources_1/new/MEM/Mem_Unit.v
+++ sources_1/new/MEM/Mem_Unit.v
@@ -1,17 +1,18 @@
-module mem_unit(clk, resetb, Instr_valid, MemRead, MemWrite, shared_global_bar, PAM, rs_data, offset, rt_data, reg_addr, neg_feedback_valid_o, neg_feedback_warpID_o, neg_feedback_scbID_o, pos_feedback_valid_o, pos_feedback_warpID_o, pos_feedback_scbID_o, pos_feedback_PAM_o, cdb_write_data, cdb_write_mask, cdb_reg_addr, cdb_regwrite)
+module mem_unit(clk, rst, Instr_valid, MemRead, MemWrite, shared_global_bar, PAM, rs_data, offset, rt_data, reg_addr, neg_feedback_valid_o, neg_feedback_warpID_o, neg_feedback_scbID_o, pos_feedback_valid_o, pos_feedback_warpID_o, pos_feedback_scbID_o, pos_feedback_PAM_o, cdb_write_data, cdb_write_mask, cdb_reg_addr, cdb_regwrite)
 	
-	input clk, resetb, Instr_valid, MemRead, MemWrite, shared_global_bar;
-	input [7:0] PAM;
-	input [255:0] rs_data, rt_data;
+	// FIXME: interface with OC
+	input clk, rst, Instr_valid, MemRead_OC, MemWrite_OC, Shared_Globalbar_OC;
+	input [7:0] PAM_OC;
+	input [255:0] rs_data_OC, rt_data_OC;
 	input [7:0] offset;
 	input [4:0] reg_addr;
 	
-	output neg_feedback_valid_o, pos_feedback_valid_o, cdb_regwrite;
-	output [2:0] neg_feedback_warpID_o, pos_feedback_warpID_o;
-	output [1:0] neg_feedback_scbID_o, pos_feedback_scbID_o;
-	output [7:0] pos_feedback_PAM_o, cdb_write_mask;
-	output [255:0] cdb_write_data;
-	output [4:0] cdb_reg_addr;
+	output ZeroFB_valid_MEM_IB, PosFB_valid_MEM_IB, RegWrite_MEM_CDB;
+	output [2:0] ZeroFB_warpID_MEM_IB, PosFB_warpID_MEM_IB;
+	// output [1:0] neg_feedback_scbID_o, pos_feedback_scbID_o; no longer need this
+	output [7:0] PosFB_MEM_IB, AM_MEM_CDB;
+	output [255:0] write_data_MEM_CDB;
+	output [4:0] reg_addr_MEM_CDB;
 	
 	
 endmodule
